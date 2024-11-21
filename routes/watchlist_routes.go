62d8b993915c26ab4c995cf24439cda70502793a
Efe Eroğlu <210541019@firat.edu.tr>
2024-11-21 23:27:13 +0300

    Add & Remove stocks to watchlist

--- routes/watchlist_routes.go
+++ routes/watchlist_routes.go
@@ -2,12 +2,15 @@ package routes
 
 import (
 	"pulsefin/controllers"
+	"pulsefin/middleware"
 
 	"github.com/labstack/echo/v4"
 )
 
 func initWatchlistRoutes(e *echo.Echo) {
 
-	e.GET("watch-lists", controllers.GetWatchList)
+	e.GET("/watchlists", controllers.GetWatchList)
+	e.POST("/watchlist", controllers.AddToWatchlist, middleware.AuthMiddleware)
+	e.DELETE("/watchlist/:stock_symbol", controllers.DeleteToWatchlist, middleware.AuthMiddleware)
 
 }
